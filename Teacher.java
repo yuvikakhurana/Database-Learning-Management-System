764c3212471c51f964c8fa9f09f01a1e94f5caa4
BenTapp753 <102811683+BenTapp753@users.noreply.github.com>
2022-05-01 17:38:38 -0400

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,19 @@
+/**
+ * Project 4 -- Teacher
+ *
+ * This class is a subclass of the Person Class
+ * and defines the Teacher users of the program.
+ *
+ * @author Yuvika Khurana, lab sec 023
+ *
+ * @version April 10, 2022
+ */
+public class Teacher extends Person {
+    //Subclass of Person class
+
+    //Teacher Constructor
+    public Teacher(String name, String username, String password) {
+        super(name, username, password);
+    }
+
+}
